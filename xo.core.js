956f7fbbf3a17d9c01c79376bbf4c4cfe0fdd3b1
meodien99 <seb.madcat@gmail.com>
2015-10-27 23:54:53 +0700

    commit base object oriented (oo) class and test file

--- xo.core.js
+++ xo.core.js
@@ -6,9 +6,11 @@
         VERSION : '0.0.1'
     };
 
+
     if(global.xo) {
         throw new Error('Xo has already been defined');
     } else {
         global.xo = xo;
     }
 })(typeof window === 'undefined' ? this : window);
+
---
+++ xo.oo.js
@@ -0,0 +1,111 @@
+/**
+ * Created by madcat on 10/27/15.
+ *
+ * The XO Class
+ *
+ *  User = xo.Class({
+ *      initialize : function(name, ago) {
+ *          this.name = name;
+ *          this.ago = ago;
+ *      }
+ *  });
+ *
+ *  new User('Mad Cat', 22);
+ *
+ * Inheritance
+ *
+ * Pass an object to XO Class to inherit from it
+ *
+ *  SuperUser = xo.Class(User, {
+ *      initialize : function(){
+ *          this.$super('initialize', arguments);
+ *      },
+ *
+ *      toString : function(){
+ *          return "SuperUser : " + this.$super('toString');
+ *      }
+ *  });
+ *
+ * Mixin
+ *
+ * Objects can be embedded within each other :
+ *
+ *  MixinUser = xo.Class({
+ *      include : User,
+ *
+ *      initialize : function(log){
+ *          this.log = log;
+ *      }
+ *  });
+ *
+ **/
+
+var Class, oo;
+Class = function(){
+    return oo.create.apply(this, arguments);
+};
+
+oo = {
+
+    $super : function(parentClass, instance, method, args) {
+        return parentClass[method].apply(instance, args);
+    },
+
+    extend : function(destination, source) {
+        for(var property in source) {
+            destination[property] = source[property];
+        }
+
+        return destination;
+    },
+
+    create : function() {
+        var methods = null,
+            parent = undefined,
+            aClass = function(){
+                this.$super = function(method, args) {
+                    return oo.$super(this.$parent, this, method, args);
+                };
+                this.initialize.apply(this, arguments);
+            };
+
+        if(typeof arguments[0] === 'function') {
+            parent = arguments[0];
+            methods = arguments[1];
+        } else {
+            methods = arguments[0];
+        }
+
+        if(typeof parent !== 'undefined') {
+            oo.extend(aClass.prototype, parent.prototype);
+            aClass.prototype.$parent = parent.prototype;
+        }
+
+        oo.mixin(aClass, methods);
+        // copy the passed in methods
+        oo.extend(aClass.prototype, methods);
+
+        // set the constructor
+        aClass.prototype.constructor = aClass;
+
+        // if there's no initialize method, set an empty one
+        if(!aClass.prototype.initialize) {
+            aClass.prototype.initialize = function (){};
+        }
+
+        return aClass;
+    },
+
+    mixin : function(aClass, methods) {
+        if(typeof methods.include !== 'undefined') {
+            if(typeof methods.include === 'function' ) {
+                oo.extend(aClass.prototype, methods.include.prototype);
+            } else {
+                for(var i = 0; i < methods.include.length; i ++) {
+                    oo.extend(aClass.prototype, methods.include[i].prototype);
+                }
+            }
+        }
+    }
+};
+
