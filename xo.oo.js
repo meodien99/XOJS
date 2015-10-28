e7e6a4e6dcfa755c833da42ae7616996cb387b83
meodien99 <seb.madcat@gmail.com>
2015-10-28 13:42:36 +0700

    modify oo class

--- xo.oo.js
+++ xo.oo.js
@@ -40,72 +40,80 @@
  *
  **/
 
-var Class, oo;
-Class = function(){
-    return oo.create.apply(this, arguments);
-};
+define('xo.oo', ['xo.core'], function(xo){
+    var Class, oo;
+    Class = function(){
+        return oo.create.apply(this, arguments);
+    };
 
-oo = {
+    oo = {
 
-    $super : function(parentClass, instance, method, args) {
-        return parentClass[method].apply(instance, args);
-    },
+        $super : function(parentClass, instance, method, args) {
+            return parentClass[method].apply(instance, args);
+        },
 
-    extend : function(destination, source) {
-        for(var property in source) {
-            destination[property] = source[property];
-        }
+        extend : function(destination, source) {
+            for(var property in source) {
+                if(source.hasOwnProperty(property))
+                    destination[property] = source[property];
+            }
 
-        return destination;
-    },
+            return destination;
+        },
 
-    create : function() {
-        var methods = null,
-            parent = undefined,
-            aClass = function(){
-                this.$super = function(method, args) {
-                    return oo.$super(this.$parent, this, method, args);
+        create : function() {
+            var methods = null,
+                parent = undefined,
+                aClass = function(){
+                    this.$super = function(method, args) {
+                        return oo.$super(this.$parent, this, method, args);
+                    };
+                    this.initialize.apply(this, arguments);
                 };
-                this.initialize.apply(this, arguments);
-            };
 
-        if(typeof arguments[0] === 'function') {
-            parent = arguments[0];
-            methods = arguments[1];
-        } else {
-            methods = arguments[0];
-        }
-
-        if(typeof parent !== 'undefined') {
-            oo.extend(aClass.prototype, parent.prototype);
-            aClass.prototype.$parent = parent.prototype;
-        }
-
-        oo.mixin(aClass, methods);
-        // copy the passed in methods
-        oo.extend(aClass.prototype, methods);
-
-        // set the constructor
-        aClass.prototype.constructor = aClass;
-
-        // if there's no initialize method, set an empty one
-        if(!aClass.prototype.initialize) {
-            aClass.prototype.initialize = function (){};
-        }
-
-        return aClass;
-    },
-
-    mixin : function(aClass, methods) {
-        if(typeof methods.include !== 'undefined') {
-            if(typeof methods.include === 'function' ) {
-                oo.extend(aClass.prototype, methods.include.prototype);
+            if(typeof arguments[0] === 'function') {
+                parent = arguments[0];
+                methods = arguments[1];
             } else {
-                for(var i = 0; i < methods.include.length; i ++) {
-                    oo.extend(aClass.prototype, methods.include[i].prototype);
+                methods = arguments[0];
+            }
+
+            if(typeof parent !== 'undefined') {
+                oo.extend(aClass.prototype, parent.prototype);
+                aClass.prototype.$parent = parent.prototype;
+            }
+
+            oo.mixin(aClass, methods);
+            // copy the passed in methods
+            oo.extend(aClass.prototype, methods);
+
+            // set the constructor
+            aClass.prototype.constructor = aClass;
+
+            // if there's no initialize method, set an empty one
+            if(!aClass.prototype.initialize) {
+                aClass.prototype.initialize = function (){};
+            }
+
+            return aClass;
+        },
+
+        mixin : function(aClass, methods) {
+            if(typeof methods.include !== 'undefined') {
+                if(typeof methods.include === 'function' ) {
+                    oo.extend(aClass.prototype, methods.include.prototype);
+                } else {
+                    for(var i = 0; i < methods.include.length; i ++) {
+                        oo.extend(aClass.prototype, methods.include[i].prototype);
+                    }
                 }
             }
         }
-    }
-};
+    };
+
+    xo.Class = Class;
+    xo.oo = oo;
+    return oo;
+});
+
 
