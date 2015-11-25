2c9f6233ce2795c436b9a2bd10d153328be64bf9
meodien99 <seb.madcat@gmail.com>
2015-11-25 15:47:45 +0700

    commit require helper, reade.me

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # XOJS
 XO.js Javascript framework like Jquery framework
+
+#### About ####
+
+XO is a javascript framework
+
+Features included :
+
+ * Classes, with extend and mixin
+ * Events
+ * Animation
+ * AMD support
+ * Chained design
+ * Function programing methods : map, each, enum ....
+
+### Basic usage ###
+Define a class just simple as :
+  ```html
+ var User = xo.Class({
+          initialize : function(name, ago) {
+              this.name = name;
+             this.ago = ago;
+        }
+     });
+```
+ And then feel free to call :
+ ```html
+  var user = new User("Adam");
+ ```
+### Inheritance  ###
+ ```html
+ SuperUser = xo.Class(User, {
+       initialize : function(){
+           this.$super('initialize', arguments);
+       },
+
+       toString : function(){
+           return "SuperUser : " + this.$super('toString');
+       }
+   });
+ ```
+### Mixin##
+ Objects can be embedded within each other :
+ ```html
+ MixinUser = xo.Class({
+      include : User,
+      initialize : function(log){
+          this.log = log;
+    }
+  });
+ ```
--- xo.require.js
+++ xo.require.js
@@ -11,7 +11,7 @@ define('xo.require',['xo.core'], function(xo){
     /**
      * Used to determine if a script is from the same origin.
      *
-     * @param {String} Path to a script
+     * @param {String} src Path to a script
      * @return {Boolean} True when `src` is from the same origin
      */
     function isSameOrigin(src){
@@ -23,7 +23,7 @@ define('xo.require',['xo.core'], function(xo){
      *
      * Options may include: `async`, `defer`, `src`, and `text`.
      *
-     * @param {Object} The options
+     * @param {Object} options The options
      * @return {Object} The script tag's DOM object
      */
     function createScript(options){
@@ -43,9 +43,85 @@ define('xo.require',['xo.core'], function(xo){
     /**
      * Inserts a script tag into the document.
      *
-     * @param {String} The script tag
+     * @param script {String} The script tag
      */
     function insertScript(script) {
         appendTo.insertBefore(script, appendTo.firstChild);
+    }
+
+
+    /**
+     * Loads scripts using XMLHttpRequest.
+     *
+     * @param scriptSrc {String} The script path
+     * @param options {Object} A configuration object
+     * @param fn {Function} A callback
+     */
+    function requireWithXMLHttpRequest(scriptSrc, options, fn){
+        if(!isSameOrigin(scriptSrc)){
+            throw ('Script load with XMLHttpRequest must be from the same origin');
+        }
+
+        if(!xo.get){
+            throw('Loading scripts with XMLHttpRequest requires xo.net to be loaded');
+        }
+
+        xo
+            .get(scriptSrc)
+            .end(function(res){
+                options.text = res.responseText;
+            });
+    }
+
+    function Queue(sources){
+        this.sources = sources;
+        this.events = new xo.events.Emitter();
+        this.queue = [];
+        this.currentGroup = 0;
+        this.groups = {};
+        this.groupKeys = [];
+        this.parseQueue(this.sources, false, 0);
+
+        this.pointer = 0;
+        this.preloadCount = 0;
+
+        var self = this;
+    }
+
+    Queue.prototype = {
+        parseQueue : function(sources, async, level){
+            var i, source;
+            for(i = 0; i < sources.length; i++){
+                source = sources[i];
+                if(xo.isArray(source)){
+                    this.currentGroup++;
+                    this.parseQueue(source, true, level + 1);
+                } else {
+                    if( level === 0 ){
+                        this.currentGroup++;
+                    }
+                    this.enqueue(source, async);
+                }
+            }
+        },
+
+        enqueue : function(source, async){
+            var preload = isSameOrigin(source);
+            var options;
+
+            options = {
+                src : source,
+                async : async,
+                preload : preload,
+                group : this.currentGroup
+            };
+
+            if(!this.groups[this.currentGroup]){
+                this.groups[this.currentGroup] = [];
+                this.groupKeys.push(this.currentGroup);
+            }
+
+            this.groups[this.currentGroup].push(options);
+        }
     }
 });
